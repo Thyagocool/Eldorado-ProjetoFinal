cfd7fbbf6fc97a6a4101460785d58aa403553ec9
Thyago Oliveira <thyagocool@gmail.com>
2022-04-24 08:04:03 -0400

    Adição do Cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const { registerRoutes } = require('./routes')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
-
+app.use(cors())
 registerRoutes(app)
 
 module.exports = app
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,9 +2,8 @@ const routes = require('./routes/')
 const UserService = require('./services/UserService')
 
 const authorization = UserService.authorization
-
-console.log('--------------------', authorization(req, res))
 const registerRoutes = (app) => {
+
     for (const route of routes) {
       app.use(route.name, route.router)
     }
